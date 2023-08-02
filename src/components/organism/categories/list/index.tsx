94b48150ef93d921da5bd77942507dcb5af344b9
Sajad Khazraee Ganjifar <sajadganjifar@gmail.com>
2023-08-02 21:45:39 +0330

    responsive the categories

--- src/components/organism/categories/list/index.tsx
+++ src/components/organism/categories/list/index.tsx
@@ -29,11 +29,11 @@ const CategoriesList = () => {
     <Stack className="justify-between w-full ">
       {FAKE_DATA.map((category) => (
         <Link key={category.title} href={`${PRODUCTS}?category=${category.title}`}>
-          <Stack className="bg-primary-light justify-between pl-5 rounded-2xl overflow-hidden sm:w-[265px] sm:h-[190px]">
-            <Typography data-testid="category-title" variant="p" className="mt-12 text-[18px]">
+          <Stack className="bg-primary-light rounded-2xl sm:overflow-hidden px-6 py-2 sm:p-0 sm:pl-5 sm:justify-between sm:w-[265px] sm:h-[190px]">
+            <Typography data-testid="category-title" variant="p" className="sm:mt-12 sm:text-[18px]">
               {category.title.toUpperCase()}
             </Typography>
-            <Image src={category.img} width={120} height={120} alt={category.title} className="flex-1" />
+            <Image src={category.img} width={120} height={120} alt={category.title} className="flex-1 hidden sm:flex" />
           </Stack>
         </Link>
       ))}
