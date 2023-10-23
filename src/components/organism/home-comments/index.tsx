5c96da579063c2853956bbcd182dfd7b0d30901c
Sajad Khazraee Ganjifar <sajadganjifar@gmail.com>
2023-10-23 22:04:27 +0330

    add scroll button for testimonial list and related product list

--- src/components/organism/home-comments/index.tsx
+++ src/components/organism/home-comments/index.tsx
@@ -1,24 +1,24 @@
-import React from "react"
+"use client"
 import Link from "next/link"
 import Stack from "@atom/stack"
+import React, { useRef } from "react"
 import { PRODUCTDETAILS } from "routes"
-import Typography from "@atom/typography"
+import HorizontalList from "@module/horizontal-list"
 import { FAKE_COMMENT } from "@src/common/fake-data"
 import HomeCommentCard from "@module/home-comment-card"
 
 const HomeComments = () => {
+  const testimonialListRef = useRef(null)
   return (
-    <Stack className="flex-col items-start md:space-y-10 w-full">
-      <Typography variant="h5">Testimonials</Typography>
-
-      <Stack className="flex-row w-full space-x-4 py-4 justify-start overflow-auto">
+    <HorizontalList ref={testimonialListRef} title="Testimonials">
+      <Stack ref={testimonialListRef} className="flex-row w-full space-x-4 py-4 justify-start overflow-auto">
         {FAKE_COMMENT.map((item) => (
           <Link key={item.productId + item.name} href={PRODUCTDETAILS(item.productId)}>
             <HomeCommentCard data={item} key={item.name + item.rate} />
           </Link>
         ))}
       </Stack>
-    </Stack>
+    </HorizontalList>
   )
 }
 
